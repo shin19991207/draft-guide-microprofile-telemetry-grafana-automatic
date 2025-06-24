522f1ee0bbaafcf480d486b69498594fdc7102ea
Morgan Chang <shin19991207@gmail.com>
2025-06-24 14:05:16 -0400

    Update to use maven wrapper and address a few comments in README

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -euxo pipefail
 
+./mvnw -version
+
 testApp() {
-    mvn -ntp -Dhttp.keepAlive=false \
+    ./mvnw -ntp -Dhttp.keepAlive=false \
         -Dmaven.wagon.http.pool=false \
         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
         -pl system,inventory -q clean package liberty:create liberty:install-feature liberty:deploy
 
-    mvn -ntp -pl system,inventory liberty:start
+    ./mvnw -ntp -pl system,inventory liberty:start
 
-    mvn -ntp -Dhttp.keepAlive=false \
+    ./mvnw -ntp -Dhttp.keepAlive=false \
         -Dmaven.wagon.http.pool=false \
         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
         -pl system,inventory failsafe:integration-test
 
-    mvn -ntp -pl system,inventory liberty:stop
+    ./mvnw -ntp -pl system,inventory liberty:stop
 
-    mvn -ntp -pl system,inventory failsafe:verify
+    ./mvnw -ntp -pl system,inventory failsafe:verify
 }
 
 testApp
